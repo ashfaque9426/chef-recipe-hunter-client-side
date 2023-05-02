a408b8c92a1dc30daac165793d3f0779078a703d
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-05-02 17:47:11 +0600

    banner section added with background image and mobile responsiveness

--- src/components/Banners/HomePageBanner/HomePageBanner.jsx
+++ src/components/Banners/HomePageBanner/HomePageBanner.jsx
@@ -1,10 +1,12 @@
-/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePageBanner = () => {
     return (
-        <section role='banner'>
-            
+        <section style={{ height: 'calc(100vh - 20vh)' }} role='banner' className='d-flex flex-column justify-content-center align-items-center text-center px-2'>
+            <h1>Welcome to our The Food Artisan</h1>
+            <Link to='/blog'><button style={{ backgroundColor: 'var(--secondary-background)' }} className='btn text-dark fs-3 fw-bold my-3'>Explore More</button></Link>
         </section>
     );
 };
--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import NavigationBar from '../NavigationBar/NavigationBar';
-
+import HomePageBanner from '../../Banners/HomePageBanner/HomePageBanner';
+import chefImg from '../../../assets/banner-images/chefImg.jpg';
+import '../Header/Header.css';
 const Header = () => {
+    const location = useLocation();
     return (
-        <header>
-            <section className='d-flex flex-column flex-md-row justify-content-md-around align-items-center py-3 gap-3' role='navigation'>
+        <header style={location.pathname === '/' ? { backgroundImage: `url(${chefImg})`, backgroundSize: 'cover', backgroundPosition: "center" } : { backgroundColor: "var(--primary-background)"}}>
+            <section className='d-flex flex-column flex-md-row justify-content-md-around align-items-center py-3 gap-3 myNav' role='navigation'>
                 <h1><Link className='text-decoration-none text-white fs-3 fw-bold' to='/'>The Food Artisan</Link></h1>
                 <NavigationBar />
                 <section className='d-flex flex-column flex-md-row gap-3 justify-content-center align-items-center'>
@@ -14,7 +17,11 @@ const Header = () => {
                     <Link to='/login'><button style={{ backgroundColor: "var(--secondary-background)"}} className='btn text-dark fw-bold'>Login</button></Link>
                 </section>
             </section>
-            
+            <section>
+                {
+                    location.pathname === '/' ? <HomePageBanner /> : ""
+                }
+            </section>
         </header>
     );
 };
