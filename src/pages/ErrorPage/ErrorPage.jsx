9a6e2fdb1f7a4fe95a3392b73971fda43a7ee0e7
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-05-05 13:22:19 +0600

    configuared the error page component and added a favicon to index.html

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const {error, status} = useRouteError();
+    
     return (
-        <div>
-            404 not found
-        </div>
+        <section style={{height: '100vh'}} className='d-flex gap-2 justify-content-center align-items-center'>
+            <h2 className='fs-5 fw-bold'>{status}</h2>
+            <div style={{ borderLeft: "7px solid var(--secondary-background)"}} className='ps-2 pt-3 pb-3 text-danger fw-semibold'>{error.message}</div>
+        </section>
     );
 };
 
