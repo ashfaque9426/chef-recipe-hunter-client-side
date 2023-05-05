52d4f9f81d50918afd0a0f3cf4fe9f86f18efae6
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-05-05 13:36:38 +0600

    error page is mobile resposive now

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
+import './ErrorPage.css'
 
 const ErrorPage = () => {
     const {error, status} = useRouteError();
     
     return (
-        <section style={{height: '100vh'}} className='d-flex gap-2 justify-content-center align-items-center'>
-            <h2 className='fs-5 fw-bold'>{status}</h2>
-            <div style={{ borderLeft: "7px solid var(--secondary-background)"}} className='ps-2 pt-3 pb-3 text-danger fw-semibold'>{error.message}</div>
+        <section style={{height: '100vh'}} className='d-flex flex-column flex-md-row gap-2 justify-content-center align-items-center text-center'>
+            <h2 className='fs-5 fw-bold errStatus'>{status}</h2>
+            <div style={{ borderLeft: "7px solid var(--secondary-background)"}} className='ps-2 pt-3 pb-3 text-danger fw-semibold errMsg'>{error.message}</div>
         </section>
     );
 };
